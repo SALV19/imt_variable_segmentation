3c1ea40dbc62c37b6070a4ee895f683fedb1219e
santi <a01707122@tec.mx>
2025-06-19 21:07:37 -0600

    [FIX] working with 1 or 2 files

--- backend/src/components/home_components.ts
+++ backend/src/components/home_components.ts
@@ -41,33 +41,43 @@ export function process_data(files){
 		error: {},
 	}
 
-  const file = fs.readFileSync(files[0].path, {encoding: "utf8"});
-  
-  create_iri(iri, file)
+  const multi_file = []
+  const file_1 = fs.readFileSync(files[0].path, {encoding: "utf8"});
+  multi_file.push(file_1)
+  if (files.length > 1) {
+    let file_2 = fs.readFileSync(files[1].path, {encoding: "utf8"})
+    multi_file.push(file_2)
+  }
+
+  create_iri(iri, multi_file)
 
 	return iri
 }
 
 // Reads file and appends each parameter to 'iri' object
 // O(n)
-function create_iri(iri, file) {
+function create_iri(iri, multi_file) {
   const type = ["id", "measurements", "end", "iri"]
 	let x = 0
   let buffer = ""
+  let second_file_buffer = ""
   let end
 
-  for (let i = 0; i < file.length; i++) {
-    if (!file[i]) {
+  for (let i = 0; i < multi_file[0].length; i++) {
+    // Basecase -> EOF
+    if (!multi_file[0][i]) {
       break
     }
-    else if (file[i] == ',' && iri.id == "") {
+    // First complete parameter -> insert as id
+    else if (multi_file[0][i] == ',' && iri.id == "") {
       iri.id = buffer
       x = (x + 1) % 4
       
       buffer = ""
       continue
     }
-    else if (file[i] == ',') {
+    // Complete parameter in buffer, insert into dataset
+    else if (multi_file[0][i] == ',') {
       if (type[x] != "end") 
         iri[type[x]].push(buffer)
       else end = buffer
@@ -77,9 +87,15 @@ function create_iri(iri, file) {
       buffer = ""
       continue
     }
-    else if (file[i] == '\n') {
-      
-      iri[type[x]].push(buffer)
+    // Complete IRI -> Insert value or average of two files
+    else if (multi_file[0][i] == '\n') {
+      if (!second_file_buffer) 
+        iri[type[x]].push(buffer)
+      else {
+        const average = ((Number(buffer) + Number(second_file_buffer)) / 2).toFixed(2)
+        iri[type[x]].push(average)
+        second_file_buffer = ""
+      }
       
       x = 1
       i += iri.id.length + 1
@@ -88,9 +104,14 @@ function create_iri(iri, file) {
       continue
     }
 
-    buffer += file[i]
+    // Create buffer with read character
+    buffer += multi_file[0][i]
+    if (type[x] == "iri") {
+      second_file_buffer += multi_file[1][i]
+    }
   }
 
+  // Get distance difference and add final measurement of data
   iri.distance = iri.measurements[2] - iri.measurements[1]
   iri.measurements.push(end)
 }
--- backend/src/test/process_data.test.ts
+++ backend/src/test/process_data.test.ts
@@ -1,6 +1,19 @@
 
 import { process_data } from "../components/home_components"
 import {path, __dirname} from "../utils/import_path"
+import * as response from "./test_responses"
+// import "jest"
 
-const data = process_data([{path: path.join(__dirname, "/uploads/file-1750347001368-147064266.csv")}])
+// test("IRI object 1 file", () => {
+//   expect(process_data([
+//       {path: path.join(__dirname, "/test/file-1750347001368-147064266.csv")}, 
+//       {path: path.join(__dirname, "/test/file-1750347001368-147064266.csv")}
+//     ])).toBe(response.response_1)
+// })
+
+const data = process_data([
+  {path: path.join(__dirname, "/test/test_file_1.csv")}, 
+  {path: path.join(__dirname, "/test/test_file_2.csv")}
+])
+
 console.log(data)
