4f127415a537576a8cbe5deb43c3da7cd6f75d3e
santi <a01707122@tec.mx>
2025-06-27 11:11:10 -0600

    [ADD] front-end design upload files

--- backend/src/public/js/upload_files.js
+++ backend/src/public/js/upload_files.js
@@ -1,5 +1,68 @@
 const form = document.querySelector("#form")
+const file_input_labels = document.querySelectorAll(".label_input")
+const input_elements = document.querySelectorAll("input")
 
+// Drag and drop functionalities for file upload
+file_input_labels.forEach((labelElement) => {
+  const content = labelElement.querySelector(".insert_file")
+  content.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    content.classList.add('dragover');
+  });
+
+  content.addEventListener('dragleave', () => {
+    content.classList.remove('dragover');
+  });
+
+
+  content.addEventListener('drop', (e) => {
+    e.preventDefault()
+    content.classList.remove("dragover")
+
+    const files = e.dataTransfer.files;
+    if (files.length > 0) {
+      const input_id = labelElement.getAttribute("for")
+      const input = document.querySelector(`#${input_id}`)
+      input.files = files
+
+      input.dispatchEvent(new Event('change'))
+    }
+  })
+})
+
+// Button functionality to upload file
+function loadFile(buttonElement) {
+  const input_id = buttonElement.parentElement.parentElement.getAttribute('for')
+  const input = document.querySelector(`#${input_id}`)
+
+  input.click()
+}
+
+// Show second file input label and activate
+function show_second() {
+  const hidden_label = document.querySelector("label[for=hidden]")
+  hidden_label.classList.remove('hidden')
+  const hidden_input = document.querySelector("#hidden")
+  hidden_input.type = "file"
+}
+
+input_elements.forEach(element => {
+  const labelElement = document.querySelector(`label[for=${element.id}]`)
+  const content = labelElement.querySelector(".insert_file")
+
+  element.addEventListener('change', (e) => {
+    content.classList.remove('insert_file')
+    content.classList.add('hide')
+    
+    const preload = labelElement.querySelector(".load_file")
+    
+    preload.textContent = element.files[0].name
+    preload.classList.remove('hide')
+    preload.classList.add('insert_file')
+  })
+})
+
+// Send files
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
@@ -19,8 +82,3 @@ form.addEventListener("submit", (e) => {
     .then((res) => res.json())
     .then(json_response => create_data(json_response))
 })
-
-function show_second() {
-  const hidden_input = document.querySelector("#hidden")
-  hidden_input.type = "file"
-}
--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["../frontend/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gob: '#611232',
+        golden: '#a57f2c',
+      },
+      width: {
+        '18': '4.5rem',
+      },
+      height: {
+        '18': '4,5rem'
+      },
+      size: {
+        '18': '4.5rem'
+      },
+      fontFamily: {
+        gob: 'GMX-Regular',
+        montserrat: 'Montserrat',
+      },
+    },
   },
   plugins: [],
 }
