9c32a0c0669e989e624e23c7d5ad4a417fb3534e
santi <a01707122@tec.mx>
2025-08-16 13:58:24 -0600

    [FIX] check if there are singular points before plotting

--- backend/src/python/generate_excel.py
+++ backend/src/python/generate_excel.py
@@ -2,7 +2,9 @@ from io import BytesIO
 from openpyxl import Workbook
 from openpyxl.chart import ScatterChart, Reference, Series
 from openpyxl.chart.text import RichText
+from openpyxl.chart.series import SeriesLabel
 from openpyxl.drawing.text import  RichTextProperties,Paragraph,ParagraphProperties, CharacterProperties
+from openpyxl.styles import Font
 import json
 import sys
 
@@ -25,8 +27,6 @@ slope_values: map = map(lambda item :
 wb = Workbook()
 virtual_workbook = BytesIO()
 
-ws2 = wb.create_sheet('Datos')
-
 ws = wb.active
 
 ws.title = "Iri de segmentación dinámico"
@@ -38,29 +38,21 @@ ws["A2"] = "Inicio"
 ws["B2"] = "Fin"
 ws["C2"] = "IRI"
 
-# ws["D2"] = "Filtrado"
-# ws["E2"] = "Segmentación"
-
 ws["E2"] = "Inicio Segmento"
 ws["F2"] = "Fin Segmento"
 ws["G2"] = "IRI"
 ws["I2"] = "Puntos Singulares"
-
-ws2["A1"] = "Medicion"
-# ws2["B1"] = "Puntos Zk"
-# ws2["C1"] = "Puntos Singulares"
 
 length = len(measurements["measurements"]) + 2
 
 # Medidas generales
 for idx, measurement in enumerate(measurements["measurements"]):
   row_val = idx+3
-  new_measurement = str(measurement)[:-3] + '+' + str(measurement)[-3:]
-  new_final_measurement = measurement + (measurements["distance"])
-  new_final_measurement = str(new_final_measurement)[:-3] + '+' + str(new_final_measurement)[-3:]
-  ws2.cell(row=row_val, column=1, value=measurement)
-  ws.cell(row=row_val, column=1, value=new_measurement)
-  ws.cell(row=row_val, column=2, value=new_final_measurement)
+  end_measurement = measurement + measurements["distance"]
+  measurement_cell = ws.cell(row=row_val, column=1, value=measurement)
+  end_measurement_cell = ws.cell(row=row_val, column=2, value=end_measurement)
+  measurement_cell.number_format = '0"+"000'
+  end_measurement_cell.number_format = '0"+"000'
   ws.cell(row=row_val, column=3, value=measurements["iri"][idx])
 
 slope_values = list(slope_values)
@@ -71,15 +63,8 @@ for idx, value in enumerate(slope_values):
   ws.cell(row=row_val, column=5, value=value[0])
   ws.cell(row=row_val, column=6, value=value[1])
   ws.cell(row=row_val, column=7, value=value[2])
-
-# Segmentación por medio de IRI
-# last = None
-# idx = 2
-# for i in slope_values:
-#   idx += 1
-#   for j in range(i[0], i[1], distance):
-#     ws2.cell(row=idx, column=2, value=i[2])
-#     idx += 1
+  cell = ws.cell(row=row_val, column=8, value=value[2])
+  cell.font = Font(color="FFFFFF")
 
 # Puntos singulares
 idx = 0
@@ -91,10 +76,7 @@ for i in range(len(measurements["measurements"])):
   if (measurements["measurements"][i] == singularities[idx]["x"]):
     ws.cell(row=idx+3, column=9, value=singularities[idx]["x"])
     ws.cell(row=idx+3, column=10, value=singularities[idx]["y"])
-    # ws2.cell(row=i+1, column=3, value=singularities[idx]["y"])
     idx += 1
-  # else:
-  #   ws2.cell(row=i+1, column=3, value=None)
 
 c1 = ScatterChart()
 c1.title = "IRI segmentado"
@@ -104,42 +86,46 @@ c1.x_axis.title = "Metros"
 c1.x_axis.scaling.min = measurements["measurements"][0]
 c1.x_axis.scaling.max = measurements["measurements"][-1]
 c1.x_axis.scaling.orientation = "minMax"
+c1.x_axis.number_format = '0"+"000'
 
 c1.x_axis.majorUnit = round(len(measurements["measurements"]) / 5)
 
-measurement_values = Reference(ws2, min_col=1, min_row=3, max_row=length)
+measurement_values = Reference(ws, min_col=1, min_row=3, max_row=length)
+
 iri = Reference(ws, min_col=3, min_row=3, max_row=length)
-x_singular_points = Reference(ws, min_col=9, min_row=1, max_row=singularities_length)
-y_singular_points = Reference(ws, min_col=10, min_row=1, max_row=singularities_length)
-
-segmentation_length = len(slope_values)
-
-x_segmentation_series = []
-y_segmentation_series = []
-for i in range(segmentation_length):
-  x_segmentation_series
-  y_segmentation_series.append(Reference(ws, min_col=7, min_row=i+3))
 
 iri_series = Series(values=iri, xvalues=measurement_values, title ="IRI")
-singular_points = Series(values=y_singular_points, xvalues=x_singular_points, title="Puntos singulares")
+iri_series.graphicalProperties.line.solidFill = "4444FF"
 
-c1.series.append(singular_points)
+def get_singular_points():
+  x_singular_points = Reference(ws, min_col=9, min_row=1, max_row=singularities_length)
+  y_singular_points = Reference(ws, min_col=10, min_row=1, max_row=singularities_length)
+
+  singular_points = Series(values=y_singular_points, xvalues=x_singular_points, title="Puntos singulares")
+  singular_points.marker.symbol = "circle"
+  singular_points.marker.graphicalProperties.solidFill = "FF0000"
+  singular_points.marker.graphicalProperties.line.solidFill = "FF0000"
+  singular_points.graphicalProperties.line.noFill = True
+  return singular_points
+
+if singularities_length:
+  singular_points = get_singular_points()
+  
+  c1.series.append(singular_points)
+
+for i in range(len(slope_values)):
+  x_segmentation_series = Reference(ws, min_col=5, max_col=6, min_row=i+3)
+  y_segmentation_series = Reference(ws, min_col=7, max_col=8, min_row=i+3)
+  segmentation_series = Series(values=y_segmentation_series, 
+                              xvalues=x_segmentation_series, title="Segmentos")
+  segmentation_series.graphicalProperties.line.solidFill = "000000"
+  segmentation_series
+  c1.series.append(segmentation_series)
+
 c1.series.append(iri_series)
-# segmentation_data = Reference(ws, min_col=4, min_row=2, max_row=length)
 
-c1.width = 25
-c1.height = 12
-
-s = c1.series[0]
-s.marker.symbol = "circle"
-s.marker.graphicalProperties.solidFill = "FF0000"
-s.marker.graphicalProperties.line.solidFill = "FF0000"
-s.graphicalProperties.line.noFill = True
-
-s = c1.series[1]
-
-s.graphicalProperties.line.solidFill = "4444FF"
-
+c1.width = 30
+c1.height = 15
 
 c1.x_axis.txPr = RichText(bodyPr=RichTextProperties(anchor="ctr",anchorCtr="1",rot="-2700000",
            spcFirstLastPara="1",vertOverflow="ellipsis",wrap="square"),
@@ -151,5 +137,3 @@ ws.add_chart(c1, "L2")
 wb.save(virtual_workbook)
 
 sys.stdout.buffer.write(virtual_workbook.getvalue())
-
-# sys.stdout.flush()
