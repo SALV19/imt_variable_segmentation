e0aa77cb0807994fd258069b48b781416a10d537
santi <a01707122@tec.mx>
2025-09-03 09:06:06 -0600

    [ADD] new button with multiple select

--- backend/src/public/js/upload_files.js
+++ backend/src/public/js/upload_files.js
@@ -1,5 +1,3 @@
-const file_input_labels = document.querySelectorAll(".label_input");
-
 $("#percentile-avg").change(() => {
   if ($("#percentile-avg").is(":checked")) {
     $("#percentile-label").text("Percentil");
@@ -10,66 +8,12 @@ $("#percentile-avg").change(() => {
   }
 });
 
-// Drag and drop functionalities for file upload
-file_input_labels.forEach((labelElement) => {
-  const content = labelElement.querySelector(".insert_file");
-  content.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    content.classList.add("dragover");
-  });
-
-  content.addEventListener("dragleave", () => {
-    content.classList.remove("dragover");
-  });
-
-  content.addEventListener("drop", (e) => {
-    e.preventDefault();
-    content.classList.remove("dragover");
-
-    const files = e.dataTransfer.files;
-    if (files.length > 0) {
-      const input_id = labelElement.getAttribute("for");
-      const input = document.querySelector(`#${input_id}`);
-      input.files = files;
-
-      input.dispatchEvent(new Event("change"));
-    }
-  });
-});
-
 // Button functionality to upload file
 function loadFile(buttonElement) {
-  const input_id =
-    buttonElement.parentElement.parentElement.getAttribute("for");
-  const input = document.querySelector(`#${input_id}`);
+  const $input = $("#files_input");
 
-  input.click();
+  $input.click();
 }
-
-// Show second file input label and activate
-function show_second(element) {
-  const id = element.id.split("_")[3] + "_" + element.id.split("_")[4];
-  const $hidden_label = $(`[for=file_upload_input_2_${id}]`);
-  $hidden_label.show();
-  const $hidden_input = $(`[id=file_upload_input_2_${id}]`);
-  $hidden_input.attr("type", "file");
-}
-
-$("[id^=file_upload_input]").each((idx, element) => {
-  const labelElement = document.querySelector(`label[for=${element.id}]`);
-  const content = labelElement.querySelector(".insert_file");
-
-  element.addEventListener("change", (e) => {
-    content.classList.remove("insert_file");
-    content.classList.add("hide");
-
-    const preload = labelElement.querySelector(".load_file");
-
-    preload.textContent = element.files[0].name;
-    preload.classList.remove("hide");
-    preload.classList.add("insert_file");
-  });
-});
 
 // Send files
 $("#iri_form, #friccion_form").on("submit", (e) => {
