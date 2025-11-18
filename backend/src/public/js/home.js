64649110c881be1af35e83a6f160a01058f78142
SALV <a01707122@tec.mx>
2025-11-18 14:39:04 -0600

    feat: add information tab

--- backend/src/public/js/home.js
+++ backend/src/public/js/home.js
@@ -2,16 +2,30 @@ $("#info").on("click", function () {
   $("#info").addClass("selected");
   $("#components").removeClass("selected");
 
-  $("#information_module").show();
-  $("#configuration_module").hide();
+  $("#info_module").show();
+  $("#components_module").hide();
 });
 
 $("#components").on("click", function () {
   $("#info").removeClass("selected");
   $("#components").addClass("selected");
 
-  $("#information_module").hide();
-  $("#configuration_module").show();
+  $("#info_module").hide();
+  $("#components_module").show();
+});
+
+$("button[id^=component_]").on("click", function (event) {
+  $("button[id^=component_]").each((idx, e) => {
+    const id = e.id.split("component_")[1];
+
+    if (event.target.id == e.id) {
+      e.classList.add("selected");
+      $(`div[id=${id}_module]`).show("selected");
+    } else {
+      e.classList.remove("selected");
+      $(`div[id=${id}_module]`).hide("selected");
+    }
+  });
 });
 
 $("p[id^=label]").on("click", (event) => {
