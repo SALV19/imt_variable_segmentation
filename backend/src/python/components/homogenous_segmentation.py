7babe93652ae0cc985981990791b1c17b01b3ff8
SALV <a01707122@tec.mx>
2025-12-17 22:15:46 -0600

    feat: create emtpy formatted chart, missing series

--- backend/src/python/components/homogenous_segmentation.py
+++ backend/src/python/components/homogenous_segmentation.py
@@ -1,7 +1,16 @@
 from openpyxl import Workbook
 from openpyxl.styles import Font
-from openpyxl.chart import ScatterChart, Reference, Series
-from openpyxl.chart.error_bar import ErrorBars
+from openpyxl.chart.text import RichText, Text
+from openpyxl.chart.title import Title
+from openpyxl.chart.layout import Layout, ManualLayout
+from openpyxl.drawing.text import (
+    Paragraph,
+    ParagraphProperties,
+    RegularTextRun,
+    CharacterProperties,
+    Font as PlotFont,
+)
+from openpyxl.chart import ScatterChart
 
 
 def homogenous_segmentation(wb: Workbook, h_segmentation):
@@ -38,13 +47,59 @@ def homogenous_segmentation(wb: Workbook, h_segmentation):
             else:
                 parameters[parameter].append((segment["end"], value))
 
+    series = {}
     for idx, (parameter, position_value) in enumerate(parameters.items()):
         # cell = ws.cell(row=2, column=18 + idx * 2 + 1, value=parameter)
         # cell.font = Font(color="FFFFFF")
+        x_series_cells = []
+        idx_series_cell = []
         for parameter_idx, (position, value) in enumerate(position_value):
-            cell = ws.cell(row=parameter_idx + 3, column=18 + idx * 2, value=position)
-            cell.font = Font(color="FFFFFF")
-            cell = ws.cell(
+            position_cell = ws.cell(
+                row=parameter_idx + 3, column=18 + idx * 2, value=position
+            )
+            position_cell.font = Font(color="FFFFFF")
+
+            idx_cell = ws.cell(
                 row=parameter_idx + 3, column=18 + idx * 2 + 1, value=idx + 1
             )
-            cell.font = Font(color="FFFFFF")
+            idx_cell.font = Font(color="FFFFFF")
+
+            x_series_cells.append(position_cell)
+            idx_series_cell.append(idx_cell)
+
+        series["parameter"] = {
+            "x_series_cells": x_series_cells,
+            "idx_series_cell": idx_series_cell,
+        }
+
+    chart = ScatterChart()
+
+    # Chart customization
+    chart.style = 2
+    chart.y_axis.delete = True
+    chart.roundedCorners = False
+
+    char_props = CharacterProperties(
+        sz=1400,
+        solidFill="BBBBBB",
+        latin=PlotFont(typeface="Roboto"),
+    )
+
+    paragraph = Paragraph(
+        pPr=ParagraphProperties(defRPr=char_props),
+        r=[RegularTextRun(t="Segmentación Homogenea")],
+    )
+
+    rich_text = RichText(p=[paragraph])
+    text = Text(rich=rich_text)
+    chart.title = Title(text)
+
+    chart.legend.position = "b"
+    chart.legend.layout = Layout(
+        manualLayout=ManualLayout(
+            x=0.4,
+            y=0.95,
+        )
+    )
+
+    ws.add_chart(chart, "M2")
